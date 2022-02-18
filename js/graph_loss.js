6a5dbd0c348cbeef02754be5cecacd4349f263d4
dronperminov <dronperminov@gmail.com>
2022-02-18 13:54:51 +0300

    add atan loss function

--- js/graph_loss.js
+++ js/graph_loss.js
@@ -88,6 +88,7 @@ function GraphLoss() {
     this.grad_gt = this.GradGtLoss()
     this.tanh5 = this.TanhLoss(new Constant(5))
     this.tanh100 = this.TanhLoss(new Constant(100))
+    this.atan100 = this.AtanLoss(new Constant(100))
 }
 
 GraphLoss.prototype.GradGtLoss = function() {
@@ -99,12 +100,12 @@ GraphLoss.prototype.GradGtLoss = function() {
     let dx2 = new Sub(this.real_x2, this.pred_x2)
     let dy2 = new Sub(this.real_y2, this.pred_y2)
 
-    let k1_x1 = new Sub(new Mult(HALF, new Square(dx1)), new Mult(this.pred_x1, new Sign(dx1)))
-    let k1_x2 = new Sub(new Mult(HALF, new Square(dx2)), new Mult(this.pred_x2, new Sign(dx2)))
+    let k1_x1 = new Add(new Mult(HALF, new Square(dx1)), new Mult(dx1, new Sign(dx1)))
+    let k1_x2 = new Add(new Mult(HALF, new Square(dx2)), new Mult(dx2, new Sign(dx2)))
     let k1_x = new Div(new Add(k1_x1, k1_x2), real_width)
 
-    let k1_y1 = new Sub(new Mult(HALF, new Square(dy1)), new Mult(this.pred_y1, new Sign(dy1)))
-    let k1_y2 = new Sub(new Mult(HALF, new Square(dy2)), new Mult(this.pred_y2, new Sign(dy2)))
+    let k1_y1 = new Add(new Mult(HALF, new Square(dy1)), new Mult(dy1, new Sign(dy1)))
+    let k1_y2 = new Add(new Mult(HALF, new Square(dy2)), new Mult(dy2, new Sign(dy2)))
     let k1_y = new Div(new Add(k1_y1, k1_y2), real_height)
 
     return new Add(k1_x, k1_y)
@@ -131,23 +132,73 @@ GraphLoss.prototype.TanhLoss = function(k) {
     return new Add(Lx, Ly)
 }
 
+GraphLoss.prototype.AtanLossCoef = function(k, x) {
+    return new Mult(TWO_OVER_PI, new Sub(new Mult(x, new Atan(new Mult(k, x))), new Div(new Log(new Add(ONE, new Square(new Mult(k, x)))), new Mult(TWO, k))))
+}
+
+GraphLoss.prototype.AtanLoss = function(k, needL2) {
+    let real_width = new Sub(this.real_x2, this.real_x1)
+    let real_height = new Sub(this.real_y2, this.real_y1)
+
+    let dx1 = new Sub(this.real_x1, this.pred_x1)
+    let dy1 = new Sub(this.real_y1, this.pred_y1)
+    let dx2 = new Sub(this.real_x2, this.pred_x2)
+    let dy2 = new Sub(this.real_y2, this.pred_y2)
+
+    let k_x1 = this.AtanLossCoef(k, dx1)
+    let k_x2 = this.AtanLossCoef(k, dx2)
+
+    let k_y1 = this.AtanLossCoef(k, dy1)
+    let k_y2 = this.AtanLossCoef(k, dy2)
+
+    let Lx = new Div(new Add(k_x1, k_x2), real_width)
+    let Ly = new Div(new Add(k_y1, k_y2), real_height)
+
+    return new Add(Lx, Ly)
+}
+
 GraphLoss.prototype.GradLoss = function() {
     let real_width = this.real_x2.value - this.real_x1.value
     let real_height = this.real_y2.value - this.real_y1.value
+
+    let pred_width = this.pred_x2.value - this.pred_x1.value
+    let pred_height = this.pred_y2.value - this.pred_y1.value
+
+    let min_width = Math.min(real_width, pred_width)
+    let max_width = Math.max(real_width, pred_width)
+
+    let min_height = Math.min(real_height, pred_height)
+    let max_height = Math.max(real_height, pred_height)
+
+    // int nodes
+    let int_x1 = Math.max(this.real_x1.value, this.pred_x1.value)
+    let int_x2 = Math.min(this.real_x2.value, this.pred_x2.value)
+    let int_y1 = Math.max(this.real_y1.value, this.pred_y1.value)
+    let int_y2 = Math.min(this.real_y2.value, this.pred_y2.value)
+
+    let int_width = int_x2 - int_x1
+    let int_height = int_y2 - int_y1
+
+    // convex nodes
+    let convex_x1 = Math.min(this.real_x1.value, this.pred_x1.value)
+    let convex_x2 = Math.max(this.real_x2.value, this.pred_x2.value)
+    let convex_y1 = Math.min(this.real_y1.value, this.pred_y1.value)
+    let convex_y2 = Math.max(this.real_y2.value, this.pred_y2.value)
+
+    let convex_width = convex_x2 - convex_x1
+    let convex_height = convex_y2 - convex_y1
 
     let dx1 = this.real_x1.value - this.pred_x1.value
     let dy1 = this.real_y1.value - this.pred_y1.value
     let dx2 = this.real_x2.value - this.pred_x2.value
     let dy2 = this.real_y2.value - this.pred_y2.value
 
-    let k = 25
-
     return {
         loss: Math.abs(dx1) + Math.abs(dy1) + Math.abs(dx2) + Math.abs(dy2),
-        dx1: -(Math.tanh(k * dx1) + dx1) / real_width,
-        dy1: -(Math.tanh(k * dy1) + dy1) / real_height,
-        dx2: -(Math.tanh(k * dx2) + dx2) / real_width,
-        dy2: -(Math.tanh(k * dy2) + dy2) / real_height
+        dx1: 1 / convex_width * ((1 - Math.sign(dx1)) / 2 - (1 + Math.sign(dx1)) / 2 * (int_width + 1) / (convex_width + 1)),
+        dy1: 1 / convex_height * ((1 - Math.sign(dy1)) / 2 - (1 + Math.sign(dy1)) / 2 * (int_height + 1) / (convex_height + 1)),
+        dx2: 1 / convex_width * ((1 - Math.sign(dx2)) / 2 * (int_width + 1) / (convex_width + 1) - (1 + Math.sign(dx2)) / 2),
+        dy2: 1 / convex_height * ((1 - Math.sign(dy2)) / 2 * (int_height + 1) / (convex_height + 1) - (1 + Math.sign(dy2)) / 2)
     }
 }
 
@@ -196,6 +247,9 @@ GraphLoss.prototype.Evaluate = function(realBox, predBox, name) {
     }
     else if (name == 'tanh100') {
         loss = this.tanh100
+    }
+    else if (name == 'atan100') {
+        loss = this.atan100
     }
     else
         throw "unknown loss '" + name + "'"
